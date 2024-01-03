d7efee9ab40c5cac8b64f63e00e38855334175d0
athuljain <athuljainkj2@gmail.com>
2024-01-03 10:23:09 +0530

    BackEnd

--- BackEnd/Server.js
+++ BackEnd/Server.js
@@ -1,1 +1,22 @@
 const express=require('express')
+const mongoose=require('mongoose')
+require('dotenv').config()
+const app=express()
+
+app.use(express.json())
+
+
+mongoose.connect(process.env.MONGODB_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+.then(() => {
+    console.log("data base connect");
+  })
+  .catch(() => {
+    console.log("connection failed");
+  });
+
+  app.listen(5000,()=>{
+    console.log('Server is Running on port 5000');
+  })
